86af7277f470ddedb7f53e359832ea3d66566ab1
ciuffochris <chris.ciuffo@gmail.com>
2022-08-23 23:05:55 -0300

    resultado etapa 3

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,6 +3,4 @@ let email = 'chris.@htmail.com'
 let trabalho = 'programador'
 let idade = 31
 let lugarfavorito = 'Roma'
-console.log('Ola'); 
-console.log(nome);
-console.log ('como voce esta?')
+console.log("Olá, " +nome+ "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugarfavorito + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + trabalho + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
