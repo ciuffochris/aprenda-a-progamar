fc738509173b3d3930b0d4826fe571372821cceb
ciuffochris <chris.ciuffo@gmail.com>
2022-08-24 12:25:23 -0300

    Testando-variaveis-matematicas-basicas

---
+++ teste-conta-de-bar.js
@@ -0,0 +1,6 @@
+let valorDaConta = 250 
+let gorjeta = 0.13
+let totalConta = valorDaConta * gorjeta + valorDaConta
+console.log("Olá, o valor da sua conta é R$: " +valorDaConta+" ")
+console.log("A taxa de serviço é 13%, gostaria de ajudar o garçom?")
+console.log("O total da sua conta com o serviço é de: "+totalConta+" ")
