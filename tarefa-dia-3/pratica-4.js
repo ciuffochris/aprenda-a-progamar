dbedac35633b9fcbf80682d707dfeca569344ab8
ciuffochris <chris.ciuffo@gmail.com>
2022-08-25 14:07:03 -0300

    pratica4-numerosprimos

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,11 @@
+/* Exibindo os números primos */
+
+let number = 17
+let oito = 8
+if (number %2 == 1) {
+    console.log(`Sim, ${number} é um número primo`)
+}
+else (oito %2 == 1) ;
+    console.log(`Não, ${oito} não é um número primo`)
+;
+    
