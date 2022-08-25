519681948a3c26538a42cf59ad5c464074d7bb0c
ciuffochris <chris.ciuffo@gmail.com>
2022-08-24 22:21:25 -0300

    multiplicadores

---
+++ dia-3/pratica3.js
@@ -0,0 +1,14 @@
+/* Dominando Loops e contadores
+
+Exercicio 1: Exibindo numeros de 1 a 1000
+Crie uma funcao que quando chamada ela mostra no console os numeros de 1 a 1000 */
+
+
+
+let contador = 1
+let mil = 1000
+while (contador <= mil) { 
+    console.log(contador)
+    contador++
+ } 
+
